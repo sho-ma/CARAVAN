be13e99392c21c00577f27b5abfb622533730fba
sho-ma <shomaegawa510@gmail.com>
2021-04-06 14:19:32 +0000

    [Finish]3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,12 @@
 class BlogsController < ApplicationController
   
+  def index
+    @blogs = Blog.all
+  end
+  
   def show
     @blog = Blog.find(params[:id])
     
-  end
-  
-  def index
-    @blogs = Blog.all
   end
   
   def new
@@ -28,7 +28,13 @@ class BlogsController < ApplicationController
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
-
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
+  
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
